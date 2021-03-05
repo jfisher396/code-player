c1585b24ca6d4c8d313116284f81afd77cb82816
James Fisher <jfisher396@hotmail.com>
2021-03-05 12:58:46 -0800

    panels resize based on how many panels are in use

--- script.js
+++ script.js
@@ -11,18 +11,28 @@ $(document).ready(function () {
     }
   );
 
-  // changes button background color if selected and removes if deselected. Also, removes highlight when clicked.
+  // event listener for button clicks
   $(".toggle-button").click(function () {
+    // toggles the class "active" on and off
     $(this).toggleClass("active");
+    // removes the highlight of the button
     $(this).removeClass("highlighted-button");
+    // gets the name of the button and makes it into a variable
+    const panelId = $(this).attr("id") + "-panel";
+    // toggles the hidden class on and off
+    $("#" + panelId).toggleClass("hidden");
+    // determines the number of active panels and adjusts each's width accordingly
+    const activePanels = 4 - $('.hidden').length;
+    console.log(activePanels)
+    $(".panel").width($(window).width() / activePanels);
   });
 
   // makes the textarea height equal to window height minus the height of the header
   $("textarea").height($(window).height() - $("#header").height() - 15);
-  //makes the panels each half of the window minus 10 pixels
-  $(".panel").width($(window).width() / 2 - 10);
+
   // sets the contents of the output panel to whatever is in the html panel on page load
   $("iframe").contents().find("html").html($("#html-panel").val());
+
   // refreshes the contents of the output panel if any changes are made to the html panel
   $("textarea").on("change keyup paste", function () {
     $("iframe").contents().find("html").html($("#html-panel").val());
