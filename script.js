de127dea7daf43627a34298621ee3f849d0446b6
James Fisher <jfisher396@hotmail.com>
2021-03-05 10:06:59 -0800

    added active function to show which buttons are active

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 console.log("script.js is working");
 
 $(document).ready(function () {
+    // changes background color of buttons when hovered over
   $(".toggle-button").hover(
     function () {
-      $(this).css("background-color", "grey");
+      $(this).addClass("highlighted-button");
     },
     function () {
-      $(this).css("background-color", "#EEEEEE");
+      $(this).removeClass("highlighted-button");
     }
   );
+
+  //
+  $(".toggle-button").click(function() {
+      $(this).toggleClass("active")
+  })
 });
