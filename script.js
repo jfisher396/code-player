083602c2c0d404a67b1e956138e99ccdba1ec860
James Fisher <jfisher396@hotmail.com>
2021-03-05 11:45:01 -0800

    output panel now responds to changes in the html panel

--- script.js
+++ script.js
@@ -19,5 +19,12 @@ $(document).ready(function () {
 
   // makes the textarea height equal to window height minus the height of the header
   $("textarea").height($(window).height() - $("#header").height() - 15);
-  $(".panel").width(($(window).width() / 2) - 10);
+  //makes the panels each half of the window minus 10 pixels
+  $(".panel").width($(window).width() / 2 - 10);
+  // sets the contents of the output panel to whatever is in the html panel on page load
+  $("iframe").contents().find("html").html($("#html-panel").val());
+  // refreshes the contents of the output panel if any changes are made to the html panel
+  $("textarea").on("change keyup paste", function () {
+    $("iframe").contents().find("html").html($("#html-panel").val());
+  });
 });
